379a93cf78bb52f1276540b7da82ef75220a6e46
akatrying2 <ohathout16@gmail.com>
2023-09-10 02:34:38 -0700

    memo litty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,10 @@
  */
 int main(void)
 {
-	Size of a char: 1 byte(s)
-Size of an int: 4 byte(s)
-Size of a long int: 4 byte(s)
-Size of a long long int: 8 byte(s)
-Size of a float: 4 byte(s)
-
-printf("Size of an int: %d byte ");
-printf("Size of a long int: %ld byte");
-printf("Size of a long long int: %lld byte");
-printf("Size of a float: %f byte");
+printf("Size of a char:%d byte(s)\n)") 
+printf("Size of an int: %d byte(s)\n");
+printf("Size of a long int: %ld byte(s)\n");
+printf("Size of a long long int: %lld byte(s)\n");
+printf("Size of a float: %f byte(s)\n");
     return (0);
 }
